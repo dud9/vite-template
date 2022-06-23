51e7a6c336a71d4a5af7a82b9eca2f287234ac9a
faitsse <2138889191@qq.com>
2022-06-23 14:52:43 +0800

    chore: init router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,1 +1,15 @@
-export default {}
+import { createRouter, createWebHashHistory } from 'vue-router'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
+NProgress.configure({ showSpinner: false, easing: 'ease', speed: 500 })
+
+const router = createRouter({
+  history: createWebHashHistory('/'),
+  routes: [],
+  scrollBehavior() {
+    return { top: 0 }
+  },
+})
+
+export default router
